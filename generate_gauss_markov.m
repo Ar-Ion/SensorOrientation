8fae8c09bb884380a542374b8858a287b2b0e565
Arion <arion@innovazion.ch>
2023-03-09 10:14:40 +0100

    Updated gauss-markov definitions and changed fft scale

--- generate_gauss_markov.m
+++ generate_gauss_markov.m
@@ -3,6 +3,6 @@ function theta_n = generate_gauss_markov(length_of_noise, sigma, beta)
     theta_n = generate_white_noise(length_of_noise, sigma);
     
     for i = 2:length_of_noise
-        theta_n(i) = theta_n(i) + theta_n(i-1)*exp(-1/beta);
+        theta_n(i) = theta_n(i) + theta_n(i-1)*exp(-beta);
     end
 end
